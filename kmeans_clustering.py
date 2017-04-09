e8813d3ddf0700803298464da50bc8440d1c2cac
Archana Bajaj <bajaj@bu.edu>
2017-04-09 13:19:52 -0400

    k-means clustering algorithm and images

---
+++ kmeans_clustering.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  8 16:44:10 2017
+
+@author: Archana Bajaj
+"""
+
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+Z = cv2.imread('3.jpg')
+
+# convert to np.float32
+Z = np.float32(Z)
+
+# define criteria and apply kmeans()
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+ret,label,center=cv2.kmeans(Z,2,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+
+# Now separate the data, Note the flatten()
+A = Z[label.ravel()==0]
+B = Z[label.ravel()==1]
+
+# Plot the data
+plt.scatter(A[:,0],A[:,1])
+plt.scatter(B[:,0],B[:,1],c = 'r')
+plt.scatter(center[:,0],center[:,1],s = 80,c = 'y', marker = 's')
+plt.xlabel('Height'),plt.ylabel('Weight')
+plt.show()
