04cf390b9ea29d97cb4c7327a25e67e8a8960f0c
Aastha Anand <aastha24@bu.edu>
2017-05-04 13:44:55 -0400

    Update dinic.cpp

--- dinic.cpp
+++ dinic.cpp
@@ -2,28 +2,20 @@
 #include<bits/stdc++.h>
 using namespace std;
  
-// A structure to represent a edge between two vertex
 struct Edge
 {
-    int v ;  // Vertex v (or "to" vertex)
-             // of a directed edge u-v. "From"
-             // vertex u can be obtained using
-             // index in adjacent array.
+    int v ;  
+    int flow ; 
+    int C; 
  
-    int flow ; // flow of data in edge
- 
-    int C;    // capacity
- 
-    int rev ; // To store index of reverse
-              // edge in adjacency list so that
-              // we can quickly find it.
+    int rev ;
 };
  
 // The Residual Graph construction
 class Graph
 {
-    int V; // number of vertex
-    int *level ; // stores level of a node
+    int V; 
+    int *level ; 
     vector< Edge > *adj;
 public :
     Graph(int V)
@@ -32,37 +24,26 @@ public :
         this->V = V;
         level = new int[V];
     }
- 
-    // add edge to the graph
+
     void addEdge(int u, int v, int C)
     {
-        // Forward edge : 0 flow and C capacity
         Edge a{v, 0, C, adj[v].size()};
- 
-        // Back edge : 0 flow and 0 capacity
         Edge b{u, 0, 0, adj[u].size()};
- 
         adj[u].push_back(a);
-        adj[v].push_back(b); // reverse edge
+        adj[v].push_back(b); 
     }
  
     bool BFS(int s, int t);
     int sendFlow(int s, int flow, int t, int ptr[]);
     int DinicMaxflow(int s, int t);
 };
- 
-// Finds if more flow can be sent from s to t.
-// Also assigns levels to nodes.
+
 bool Graph::BFS(int s, int t)
 {
     for (int i = 0 ; i < V ; i++)
         level[i] = -1;
  
-    level[s] = 0;  // Level of source vertex
- 
-    // Create a queue, enqueue source vertex
-    // and mark source vertex as visited here
-    // level[] array works as visited array also.
+    level[s] = 0; 
     list< int > q;
     q.push_back(s);
  
@@ -76,57 +57,32 @@ bool Graph::BFS(int s, int t)
             Edge &e = *i;
             if (level[e.v] < 0  && e.flow < e.C)
             {
-                // Level of current vertex is,
-                // level of parent + 1
                 level[e.v] = level[u] + 1;
  
                 q.push_back(e.v);
             }
         }
     }
- 
-    // IF we can not reach to the sink we
-    // return false else true
+
     return level[t] < 0 ? false : true ;
 }
- 
-// A DFS based function to send flow after BFS has
-// figured out that there is a possible flow and
-// constructed levels. This function called multiple
-// times for a single call of BFS.
-// flow : Current flow send by parent function call
-// start[] : To keep track of next edge to be explored.
-//           start[i] stores  count of edges explored
-//           from i.
-//  u : Current vertex
-//  t : Sink
+
 int Graph::sendFlow(int u, int flow, int t, int start[])
 {
     // Sink reached
     if (u == t)
         return flow;
- 
-    // Traverse all adjacent edges one -by - one.
     for (  ; start[u] < adj[u].size(); start[u]++)
     {
-        // Pick next edge from adjacency list of u
         Edge &e = adj[u][start[u]]; 
                                      
         if (level[e.v] == level[u]+1 && e.flow < e.C)
         {
-            // find minimum flow from u to t
             int curr_flow = min(flow, e.C - e.flow);
- 
             int temp_flow = sendFlow(e.v, curr_flow, t, start);
- 
-            // flow is greater than zero
             if (temp_flow > 0)
             {
-                // add flow  to current edge
                 e.flow += temp_flow;
- 
-                // subtract flow from reverse edge
-                // of current edge
                 adj[e.v][e.rev].flow -= temp_flow;
                 return temp_flow;
             }
@@ -136,27 +92,21 @@ int Graph::sendFlow(int u, int flow, int t, int start[])
     return 0;
 }
  
-// Returns maximum flow in graph
 int Graph::DinicMaxflow(int s, int t)
 {
-    // Corner case
+    
     if (s == t)
         return -1;
  
-    int total = 0;  // Initialize result
- 
-    // Augment the flow while there is path
-    // from source to sink
+    int total = 0;  
+
     while (BFS(s, t) == true)
     {
-        // store how many edges are visited
-        // from V { 0 to V }
+
         int *start = new int[V+1];
- 
-        // while flow is not zero in graph from S to D
+
         while (int flow = sendFlow(s, INT_MAX, t, start))
- 
-            // Add path flow to overall flow
+
             total += flow;
     }
  
@@ -167,7 +117,9 @@ int Graph::DinicMaxflow(int s, int t)
 
 int main()
 {
-    Graph g(6);
+    
+ // Check from text-file input -Aastha
+ Graph g(6);
     g.addEdge(0, 1, 16 );
     g.addEdge(0, 2, 13 );
     g.addEdge(1, 2, 10 );
